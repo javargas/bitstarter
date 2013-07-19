4a4dabd2fb7944a17bafd4099f030ce881c20661
John Vargas <javargas@linux-vvdw.site>
2013-07-19 10:44:01 -0500

    Reading html file with Nodejs

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
 app.get('/', function(request, response) {
   
   var bufferHtml = fs.readFileSync('index.html');
